324029981e86ecc60d70a00258e362b98d396682
wiirtex <otim142@ya.ru>
2021-08-27 12:23:34 +0000

    arguments_read_added

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
-int main() 
+					
+int main(int argc, char *argv[] ) 
 {
+	int n;	
 	printf("check1\n");
-	int n;
-	char a[50];
-	scanf("%s", a);
-        sscanf(a, "%d", &n);
+    sscanf(argv[1], "%d", &n);
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = n-i; j > 0; j--)
